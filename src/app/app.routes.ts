ff09479e51608e8c8c1f84f6172269556bc4c9cf
Jennyfer NGUENO <jngueno@yahoo.fr>
2017-03-06 10:00:28 +0100

    Remove files useless

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home';
 import { AboutComponent } from './about';
 import { NoContentComponent } from './no-content';
 
@@ -8,10 +7,8 @@ import {ConnectionComponent} from "./connection/connection.component";
 
 export const ROUTES: Routes = [
   { path: 'connection', component: ConnectionComponent},
-  { path: '',      component: HomeComponent },
-  { path: 'home',  component: HomeComponent },
+  { path: '',      component: ConnectionComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'detail', loadChildren: './+detail#DetailModule'},
   { path: 'barrel', loadChildren: './+barrel#BarrelModule'},
   { path: '**',    component: NoContentComponent }
 ];
