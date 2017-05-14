839bd883d5027d910ea713bdb64f48b5b48c59f7
Housseini.maiga <174646@supinfo.com>
2017-05-14 20:37:33 +0200

    shelvest on regiter-form

---
+++ src/app/chattManager/audio/video.component.ts
@@ -0,0 +1,22 @@
+/**
+ * Created by Housseini  Maiga on 5/10/2017.
+ */
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+
+
+
+@Component({
+  selector: 'qwirk-video',
+  templateUrl: 'video.component.html',
+  styleUrls: ['./video.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class VideoComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+}
