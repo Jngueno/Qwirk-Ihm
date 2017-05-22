f2da08922d8e7a0073b142662ac6b10bcf8d5424
Housseini.maiga <174646@supinfo.com>
2017-05-22 21:40:28 +0200

    peer service

--- src/app/shared/services/peerConnection.service.ts
+++ src/app/shared/services/peerConnection.service.ts
@@ -2,47 +2,30 @@
  * Created by Housseini  Maiga on 5/18/2017.
  */
 import {Injectable} from "@angular/core";
+import {Http, Response} from "@angular/http";
+import {Router} from "@angular/router";
+
+//Internal import
+import 'rxjs/add/operator/map'
+import {APPCONFIG} from "../../config/param";
 
 @Injectable()
-export class PeerConnection {
-  peer : any;
-  nav  = <any>navigator;
-  initiatorPeerId;
-  joinerPeerId;
-  constructor() {
+export class PeerConnectionService {
+  private url: string;
+  private appConfig: APPCONFIG;
+  constructor(private http: Http,
+              private route: Router) {
+    this.appConfig = new APPCONFIG();
+    this.url = this.appConfig.urlAPI;
   }
 
-  requestUserMedia(peer : any){
-    this.nav.getUserMedia = ( this.nav.getUserMedia ||
-    this.nav.webkitGetUserMedia ||
-    this.nav.mozGetUserMedia ||
-    this.nav.msGetUserMedia);
-    this.nav.getUserMedia({video: true, audio: true}, function(stream) {
-      let call = peer.call('another-peers-id', stream);
-      call.on('stream', function(remoteStream) {
-        // Show stream in some video/canvas element.
-      });
-    }, function(err) {
-      console.log('Failed to get local stream' ,err);
-    });
+  updateUserPerId(peer : string) {
+    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    let user = {
+      email : currentUser,
+      peerId : peer
+    };
+    return this.http.put(this.url + 'update/peer/', user)
+      .map((response: Response) => response.json());
   }
-  onConnectPeer() {
-      let conn = this.peer.connect(this.joinerPeerId);
-      conn.on('open', function(){
-        conn.send('Message from that id');
-      });
-  }
-
-  onPeer(peer : any) {
-    peer.on('call', function(call) {
-      console.log('My peer id 3: ', peer.id);
-      // Answer automatically for demo
-      console.log('call: ', call);
-      call.answer("");
-
-    });
-  }
-
-
-
 }
